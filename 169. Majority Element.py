d5ac3e1ef2473f180380dcc006e80909ed3712f4
Monty <me@promila.info>
2021-02-11 13:26:56 +0600

    Initial commit

---
+++ 169. Majority Element.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def majorityElement(self, nums):
+        sum = {}
+        for n in nums:
+            if n not in sum:
+                sum[n]=1
+            else:
+                sum[n] += 1
+            if sum[n] > len(nums)/2:
+                return n
+
+nums = [1,1,2,3,1]
+obj=Solution()
+print(obj.majorityElement(nums))
