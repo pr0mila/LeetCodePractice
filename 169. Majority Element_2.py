921562365d26296ef257a18a952d8ee309e1a155
monty <me@promila.info>
2021-08-02 17:47:02 +0600

    regular update

--- 169. Majority Element_2.py
+++ 169. Majority Element_2.py
@@ -8,9 +8,11 @@ class Solution(object):
                 sum[n] += 1
             if sum[n] > len(nums)/2:
                 return n
-
+"""
 nums = [1,1,2,3,1]
 obj=Solution()
 print(obj.majorityElement(nums))
+
+"""
 
 
